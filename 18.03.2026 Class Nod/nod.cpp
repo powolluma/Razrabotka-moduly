ad90a659699ba352be70d4c59135477ac38c3f10
powolluma <sgdfgssdfgsergergsdfg@gmail.com>
2026-03-17 15:34:08 +0900

    Add files via upload

---
+++ 18.03.2026 Class Nod/nod.cpp
@@ -0,0 +1,7 @@
+#include "nod.h"
+int nod(int x, int y) {
+    while (x!=y)
+        if (x>y) x=x-y;
+        else y=y-x;
+    return y;
+}
---
+++ 18.03.2026 Class Nod/nod.h
@@ -0,0 +1,4 @@
+#ifndef NOD_H
+#define NOD_H
+int nod(int, int);
+#endif // NOD_H
